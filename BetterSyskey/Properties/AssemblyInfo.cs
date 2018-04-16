23939e36bcebdff79446983de5a7320880cf2ecf
bnielson <bnielson@capitalpremium.net>
2018-04-16 13:33:37 -0600

    Assembly Info
     - Updated assembly title and description

--- BetterSyskey/Properties/AssemblyInfo.cs
+++ BetterSyskey/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BetterSyskey")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Better Syskey")]
+[assembly: AssemblyDescription("Replacement for the Syskey program to annoy tech scammers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BetterSyskey")]
